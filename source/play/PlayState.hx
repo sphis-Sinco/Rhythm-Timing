b9bfd14114bc567477ba2ddeae61b3a4250b44ad
IdealistCat <mattallan.h@gmail.com>
2024-11-24 00:32:25 -0600

    comments for my future

--- source/play/PlayState.hx
+++ source/play/PlayState.hx
@@ -23,6 +23,7 @@ class PlayState extends MusicState
 
 	override public function new()
 	{
+		// set song json (unhardcode please)
 		SONG_JSON = {
 			name: "Test",
 			bpm: 150,
@@ -46,25 +47,31 @@ class PlayState extends MusicState
 			]
 		}
 
+		// set the base song stat info
 		SONG_STATS = {
 			song: SONG_JSON.name
 		}
 
+		// base stuff to get conductor workin
 		Conductor.mapBPMChanges(SONG_JSON);
 		Conductor.changeBPM(SONG_JSON.bpm);
 
+		// play the song
 		FlxG.sound.playMusic('assets/music/Test.wav', 1.0, false);
 		FlxG.sound.music.onComplete = endSong;
 		FlxG.sound.pause();
 
+		// set song position
 		Conductor.songPosition = 0;
 
+		// init songPos Text
 		songPos = new FlxText(0, 0, 0, "Hello", 16);
 		super();
 	}
 
 	override public function create()
 	{
+		// add anything that should be added that was initalized in new()
 		add(songPos);
 		
 		super.create();
@@ -72,23 +79,28 @@ class PlayState extends MusicState
 
 	override public function update(elapsed:Float)
 	{
+		// only move the songPosition if the song hasn't ended
 		if (!endedSong)
 			Conductor.songPosition += elapsed * 1000;
 
-		if (Conductor.songPosition > 0 && !startedSong)
+		// if the songPosition is more than or equal than 0 and we have not started the song
+		if (Conductor.songPosition >= 0 && !startedSong)
 		{
+			// start the song and music
 			startedSong = true;
 			FlxG.sound.music.resume();
 		}
 
-		var musicLen:Float = FlxG.sound.music.length / 1000;
+		// Song Position Info
+		var musicLen:Float = FlxG.sound.music.length / 1000; // divide by 1000 cause its in miliseconds
 		var timeLeft:Float = FlxMath.roundDecimal(musicLen - Conductor.songPosition / 1000, 0);
 
+		// this is for the countdown (when I add it)
 		if (timeLeft > musicLen)
 			timeLeft = musicLen; // countdown time doesnt add to the length
 
+		// song position text
 		var songText:String = '' + timeLeft;
-
 		songPos.text = "Song Pos: " + songText;
 
 		super.update(elapsed);
