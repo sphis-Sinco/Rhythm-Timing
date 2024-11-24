ea05ab4a9e610d7e60dccaedaa46e2179312578e
IdealistCat <mattallan.h@gmail.com>
2024-11-23 22:53:23 -0600

    result state

---
+++ source/play/results/ResultsState.hx
@@ -0,0 +1,16 @@
+package play.results;
+
+import music.MusicState;
+
+class ResultsState extends MusicState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
